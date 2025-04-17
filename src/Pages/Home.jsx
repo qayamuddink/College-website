b13b420ddf28da9c60fdb0702714b9b526d1fbea
uvais-kasganj <uvais.kasganj@gmail.com>
2025-04-17 16:53:37 +0530

    modified:   src/Pages/Home.jsx 	modified:   src/Pages/MajorEvents.jsx 	modified:   src/components/HeroSection.jsx 	modified:   src/components/Navbar.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,8 +7,8 @@ const Home = () => {
 
       <div className="mt-6 grid grid-cols-1 md:grid-cols-3 gap-4 text-justify">
         <div className="rounded-md bg-slate-300 shadow-xl items-end">
-          <div className="m-7">
-            <img src="https://images.unsplash.com/photo-1588072432836-e10032774350?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c2Nob2x8ZW58MHx8MHx8fDA%3D" alt="Event 1" className="rounded-lg shadow-md " />
+          <div className="m-[min(1.75rem,5%)] justify-center">
+            <img src="https://i.ibb.co/mV1t7KQD/1744579619781-1.jpg" alt="https://i.ibb.co/mV1t7KQD/1744579619781-1.jpg" border="0" className="rounded-lg shadow-md place-items-center" />
             <p className="mt-4 text-2xl font-semibold font-sans">Founder's Message</p>
             <p className="mt-4 indent-4">At Carbon Institute of Paramedical Science, we believe that quality education is the key to shaping the future of healthcare. Our mission is to provide students with the skills and knowledge necessary to excel in the medical field. With a strong focus on practical learning and hands-on training, we aim to produce highly skilled paramedical professionals who can serve society with dedication and compassion. We welcome you to embark on this rewarding journey with us.</p>
             <p className="text-right italic">-Founder</p>
@@ -16,8 +16,8 @@ const Home = () => {
           </div>
         </div>
         <div className="rounded-md bg-slate-300 text-base shadow-xl">
-        <div className="m-7">
-            <img src="https://images.unsplash.com/photo-1588072432836-e10032774350?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c2Nob2x8ZW58MHx8MHx8fDA%3D" alt="Event 1" className="rounded-lg shadow-md " />
+          <div className="m-[min(1.75rem,5%)]">
+            <img src="https://i.ibb.co/60f1JwvY/1724834411764-1.png" alt="1724834411764-1" border="0" className="rounded-lg shadow-md " />
             <p className="mt-4 text-2xl font-semibold font-sans">Principal's Message</p>
             <p className="mt-4 indent-4">Education in the medical field is not just about acquiring knowledge; it is about applying that knowledge to save lives and improve healthcare standards. At Carbon Institute of Paramedical Science, we ensure that our students receive the best academic guidance, practical exposure, and ethical training to excel in their careers. I invite all aspiring medical professionals to join us and take a step toward a brighter future in healthcare.</p>
             <p className="text-right italic">-Principal</p>
@@ -25,8 +25,8 @@ const Home = () => {
           </div>
         </div>
         <div className="rounded-md bg-slate-300 text-base shadow-xl">
-        <div className="m-7">
-            <img src="https://images.unsplash.com/photo-1588072432836-e10032774350?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c2Nob2x8ZW58MHx8MHx8fDA%3D" alt="Event 1" className="rounded-lg shadow-md " />
+          <div className="m-[min(1.75rem,5%)]">
+            <img src="https://i.ibb.co/wrMz8ML7/Mohammad-Fahad-2.jpg" alt="Mohammad-Fahad-2" border="0" className="rounded-lg shadow-md " />
             <p className="mt-4 text-2xl font-semibold font-sans">Chief Guest's Remarks</p>
             <p className="mt-4 indent-4">The healthcare sector is rapidly evolving, and skilled paramedical professionals play a crucial role in strengthening medical services. Institutions like Carbon Institute of Paramedical Science are instrumental in producing competent healthcare workers who can contribute significantly to the medical industry. I commend the institute's dedication to excellence and encourage students to make the most of this opportunity to serve humanity.</p>
             <p className="text-right italic">-Chief Guest</p>
@@ -45,4 +45,3 @@ export default Home;
 
 
 
-          
--- src/Pages/MajorEvents.jsx
+++ src/Pages/MajorEvents.jsx
@@ -1,22 +1,74 @@
 import React from "react";
 
+const images = [
+  { src: "https://i.ibb.co/4wFX1kfW/building-2.jpg", preview: "https://i.ibb.co/4wFX1kfW/building-2.jpg", title: "evening" },
+  { src: "https://i.ibb.co/WWqSFZPb/IMG-20250414-WA0009.jpg", preview: "https://i.ibb.co/WWqSFZPb/IMG-20250414-WA0009.jpg", title: "evening" },
+  { src: "https://i.ibb.co/xq97BvjL/IMG-20250414-WA0011.jpg", preview: "https://i.ibb.co/xq97BvjL/IMG-20250414-WA0011.jpg", title: "evening" },
+  { src: "https://i.ibb.co/3m6ZQ6bB/IMG-20250414-WA0018.jpg", preview: "https://i.ibb.co/3m6ZQ6bB/IMG-20250414-WA0018.jpg", title: "evening" },
+  { src: "https://i.ibb.co/1GGPpTM4/IMG-20250414-WA0025.jpg", preview: "https://i.ibb.co/1GGPpTM4/IMG-20250414-WA0025.jpg", title: "evening" },
+  { src: "https://i.ibb.co/gMsNypYT/IMG-20250414-WA0026.jpg", preview: "https://i.ibb.co/gMsNypYT/IMG-20250414-WA0026.jpg", title: "evening" },
+  { src: "https://i.ibb.co/jkrVRwpp/IMG-20250414-WA0029.jpg", preview: "https://i.ibb.co/jkrVRwpp/IMG-20250414-WA0029.jpg", title: "evening" },
+  { src: "https://i.ibb.co/tTxjBPPc/IMG-20250414-WA0030.jpg", preview: "https://i.ibb.co/tTxjBPPc/IMG-20250414-WA0030.jpg", title: "evening" },
+  { src: "https://i.ibb.co/ymm7F8Jn/IMG-20250414-WA0035.jpg", preview: "https://i.ibb.co/ymm7F8Jn/IMG-20250414-WA0035.jpg", title: "evening" },
+  { src: "https://i.ibb.co/ZkzBsvm/IMG-20250414-WA0048.jpg", preview: "https://i.ibb.co/ZkzBsvm/IMG-20250414-WA0048.jpg", title: "evening" },
+  { src: "https://i.ibb.co/3m4fzHr5/IMG-20250414-WA0051.jpg", preview: "https://i.ibb.co/3m4fzHr5/IMG-20250414-WA0051.jpg", title: "evening" },
+  { src: "https://i.ibb.co/mV4rYB5z/IMG-20250414-WA0052.jpg", preview: "https://i.ibb.co/mV4rYB5z/IMG-20250414-WA0052.jpg", title: "evening" },
+  { src: "https://i.ibb.co/rGDN5DXN/IMG-20250414-WA0053.jpg", preview: "https://i.ibb.co/rGDN5DXN/IMG-20250414-WA0053.jpg", title: "evening" },
+  { src: "https://i.ibb.co/9kF05swn/IMG-20250414-WA0055.jpg", preview: "https://i.ibb.co/9kF05swn/IMG-20250414-WA0055.jpg", title: "evening" },
+  // { src: "https://i.ibb.co/tg5Lg0k/IMG-20250414-WA0056.jpg", preview: "https://i.ibb.co/tg5Lg0k/IMG-20250414-WA0056.jpg", title: "evening" },
+  { src: "https://i.ibb.co/V0cn74tV/IMG-20250414-WA0057.jpg", preview: "https://i.ibb.co/V0cn74tV/IMG-20250414-WA0057.jpg", title: "evening" },
+  { src: "https://i.ibb.co/gFwTmGdc/IMG-20250414-WA0059.jpg", preview: "https://i.ibb.co/gFwTmGdc/IMG-20250414-WA0059.jpg", title: "evening" },
+  { src: "https://i.ibb.co/pvW0sW0x/IMG-20250414-WA0063.jpg", preview: "https://i.ibb.co/pvW0sW0x/IMG-20250414-WA0063.jpg", title: "evening" },
+  { src: "https://i.ibb.co/fYytQvfd/IMG-20250414-WA0065.jpg", preview: "https://i.ibb.co/fYytQvfd/IMG-20250414-WA0065.jpg", title: "evening" },
+  { src: "https://i.ibb.co/DDV7cvGb/IMG-20250414-WA0068.jpg", preview: "https://i.ibb.co/DDV7cvGb/IMG-20250414-WA0068.jpg", title: "evening" },
+  { src: "https://i.ibb.co/TGfDcfB/IMG-20250414-WA0071.jpg", preview: "https://i.ibb.co/TGfDcfB/IMG-20250414-WA0071.jpg", title: "evening" },
+  { src: "https://i.ibb.co/p8LMRn0/IMG-20250414-WA0075.jpg", preview: "https://i.ibb.co/p8LMRn0/IMG-20250414-WA0075.jpg", title: "evening" },
+  { src: "https://i.ibb.co/Qvj5kk9c/IMG-20250414-WA0077.jpg", preview: "https://i.ibb.co/Qvj5kk9c/IMG-20250414-WA0077.jpg", title: "evening" },
+  { src: "https://i.ibb.co/21W8W8nG/IMG-20250414-WA0084.jpg", preview: "https://i.ibb.co/21W8W8nG/IMG-20250414-WA0084.jpg", title: "evening" },
+  { src: "https://i.ibb.co/DgHny90r/IMG-20250414-WA0088.jpg", preview: "https://i.ibb.co/DgHny90r/IMG-20250414-WA0088.jpg", title: "evening" },
+  { src: "https://i.ibb.co/MkvNmmgp/IMG-20250414-WA0090.jpg", preview: "https://i.ibb.co/MkvNmmgp/IMG-20250414-WA0090.jpg", title: "evening" },
+  { src: "https://i.ibb.co/hFgh3Zrx/1.jpg", preview: "https://i.ibb.co/hFgh3Zrx/1.jpg", title: "evening" },
+  { src: "https://i.ibb.co/R4455vcV/2.jpg", preview: "https://i.ibb.co/R4455vcV/2.jpg", title: "evening" },
+  { src: "https://i.ibb.co/SDtMthf9/4.jpg", preview: "https://i.ibb.co/SDtMthf9/4.jpg", title: "evening" },
+  { src: "https://i.ibb.co/YM4n5Bd/6.jpg", preview: "https://i.ibb.co/YM4n5Bd/6.jpg", title: "evening" },
+  { src: "https://i.ibb.co/p6wNTyyq/13.jpg", preview: "https://i.ibb.co/p6wNTyyq/13.jpg", title: "evening" },
+  { src: "https://i.ibb.co/S9bpVyj/15.jpg", preview: "https://i.ibb.co/S9bpVyj/15.jpg", title: "evening" },
+  { src: "https://i.ibb.co/qYbZB3J8/17.jpg", preview: "https://i.ibb.co/qYbZB3J8/17.jpg", title: "evening" },
+  { src: "https://i.ibb.co/ZzHkD0KD/35.jpg", preview: "https://i.ibb.co/ZzHkD0KD/35.jpg", title: "evening" },
+  { src: "https://i.ibb.co/3yxNLsc4/IMG-20250414-WA0085.jpg", preview: "https://i.ibb.co/3yxNLsc4/IMG-20250414-WA0085.jpg", title: "evening" },
+  { src: "https://i.ibb.co/8L7Krtzj/10.jpg", preview: "https://i.ibb.co/8L7Krtzj/10.jpg", title: "evening" },
+  { src: "https://i.ibb.co/NPKWMcf/12.jpg", preview: "https://i.ibb.co/NPKWMcf/12.jpg", title: "evening" },
+  // { src: "https://i.ibb.co/QFL61d8n/building-1.jpg", preview: "https://i.ibb.co/QFL61d8n/building-1.jpg", title: "evening" },
+  // { src: "", preview: "", title: "evening" },
+  // { src: "", preview: "", title: "evening" },
+  // { src: "", preview: "", title: "evening" },
+];
+
 const MajorEvents = () => {
   return (
-    <div className="p-6 text-center">
-      <h1 className="text-3xl font-bold text-blue-900">Major Events</h1>
-      <p className="mt-3 text-lg text-gray-700">
-        Check out the major events and celebrations at our school.
-      </p>
+    <><div className="m-4 text-center">
+      <h className="font-semibold text-3xl mt-4 align-text-top ">Here are some glimpses of Grand Opening of the Institute</h>
 
-      <div className="mt-6 bg-gray-100 p-4 rounded-lg shadow-md">
-        <h2 className="text-2xl font-semibold text-blue-800">Upcoming Events</h2>
-        <ul className="mt-2 text-gray-700 text-left list-disc list-inside">
-          <li>Annual Sports Day - March 15, 2025</li>
-          <li>Independence Day Celebration - August 15, 2025</li>
-          <li>Science Fair - November 20, 2025</li>
-        </ul>
+      <div className="flex justify-center mt-4 gap-6 flex-wrap">
+        {images.map((image, index) => (
+          <div className="flex flex-wrap justify-around shadow-lg shadow-gray-300 w-[45%] md:w-[30%] h-full rounded-lg">
+            {/* w-[min(400px,45%)] */}
+            <a
+              key={index}
+              href={image.src}
+              data-lightbox="show-2"
+              data-title={image.title}
+              target="_blank"
+            >
+              <img
+                src={image.preview}
+                alt="alternative explanation"
+                className="rounded-lg block shadow-md hover:scale-105 transition-transform duration-300" />
+            </a>
+          </div>
+        ))}
       </div>
-    </div>
+    </div></>
   );
 };
 
